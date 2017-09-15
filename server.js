5d2ace7cd328c2f44df3fea831b98b06c0bcaf50
amitkumar8486 <31071995+amitkumar8486@users.noreply.github.com>
2017-09-14 23:25:21 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,122 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var crypto=require('crypto');
+var config={
+    user:'amitkumarchauhan8486',
+    database:'amitkumarchauhan8486',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+  var title=data.title;
+  var date=data.date;
+  var heading=data.heading;
+  var content=data.content;
+  
+  var htmlTemplate=`
+  <html>
+  <head>
+  <title>
+  ${title}
+  </title>
+  <meta name="viewpoint" content="width=device-width,initial-scale=1">
+  <link href="/ui/style.css" rel="stylesheet"/>
+  </head>
+  <body>
+     <div class="container">
+          <div>
+             <a href="/">home</a>
+             </div>
+             <hr/>
+             <h3>
+             ${heading}
+             </h3>
+             <div>
+             ${date.toDateString()}
+             </div>
+             <div>
+             ${content}
+             </div>
+    </div>
+ </body>
+  
+  </html>
+  
+  
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input,salt){
+    //how do we get a hash
+   var hashed= crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+   return["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+    
+}
+
+app.get('/hash/:input',function(req,res){
+   var hashedString= hash(req.params.input,'this-is-some-random-string');
+   res.send(hashedString);
+});
+
+var pool=new Pool(config);
+app.get('/test-db', function (req, res){
+//make a select request
+//return a response with result
+pool.query('SELECT * FROM test',function(err,result){
+if (err){
+    res.status(500).send(err.toString());
+}else{
+    res.send(JSON.stringify(result.rows));
+}
+});
+});
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+var names=[];
+app.get('/submit-name',function(req,res){//url: /submit-name?name=xxxxx
+    //get the name from the request
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON javascript object notation 
+    res.send(JSON.stringify(names));
+    
+});
+
+app.get('/articles/:articleName', function (req,res){
+    //articleName==article-one
+    //articles[articleName]=={}content object for article one
+   //SELECT * FROM article WHERE title='article-one'
+ pool.query("SELECT * FROM article WHERE title= $1", [req.params.articleName],function(err,result){
+     if (err){
+          res.status(500).send(err.toString());
+     }else{
+         if(result.rows.length===0){
+             res.status(404).send('article not found');
+             
+         }
+         else{
+             var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+         }
+     }
+});
 });
 
 app.get('/ui/style.css', function (req, res) {
